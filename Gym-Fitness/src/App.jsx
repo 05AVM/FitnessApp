6a8b26e0093033a95061637c8c26ad4f0088b890
Avishek <avishekmondal923@gmail.com>
2024-05-12 15:23:48 +0530

    Updated

--- Gym-Fitness/src/App.jsx
+++ Gym-Fitness/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import './App.css'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <h1>HII</h1>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
 
 export default App
